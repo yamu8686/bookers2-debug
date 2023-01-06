c666ca9915fdc88e83dc98f7ce78a462944ad610
EC2 Default User <ec2-user@ip-172-31-47-173.ap-northeast-1.compute.internal>
2023-01-06 05:46:47 +0000

    [Start]検索機能]

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
-  get 'relationships/create'
-  get 'relationships/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
 
   root to: "homes#top"
   get "home/about"=>"homes#about",as: "about"
+  get 'relationships/followings'
+  get 'relationships/followers'
+  get 'relationships/create'
+  get 'relationships/destroy'
+  
+  get "serch" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
